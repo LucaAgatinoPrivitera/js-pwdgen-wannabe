9bdb0e94d53843db059e913538734cf71d7c748b
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-17 09:42:42 +0200

    aggiunto il cancelletto che avevo dimenticato

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let CognomeUtente = prompt ("Inserisci il tuo cognome:");
 let ColorePreferito = prompt ("Inserisci il tuo colore preferito:");
 
 //Variabile password
-let password24 = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "24";
+let password24 = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24";
 
-document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "24"); //primo caso che ho fatto
+document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24"); //primo caso che ho fatto
 
 alert(password24); //nel caso dell'alert (l'ho fatto al contrario rispetto all'ordine che ha dato Luca)

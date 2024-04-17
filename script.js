429ea8f5ec3ea9ea348a47f35e7001c14aba6108
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-17 15:17:55 +0200

    inizio

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //Variabili
-let NomeUtente = prompt ("Inserisci il tuo nome:");
-let CognomeUtente = prompt ("Inserisci il tuo cognome:");
-let ColorePreferito = prompt ("Inserisci il tuo colore preferito:");
+let tariffaKM = prompt ("Inserisci il tuo nome: ");
+let sconti = prompt ("Inserisci il tuo colore preferito: ");
 
 //Variabile password
-let password24 = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24";
+let  = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24";
 
 document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24"); //primo caso che ho fatto
 
 alert(password24); //nel caso dell'alert (l'ho fatto al contrario rispetto all'ordine che ha dato Luca)
+
+
+
+let distanzaKM = prompt ("Inserisci la distanza (in KM): ");
+let eta = prompt ("Inserisci la tua età: ");
